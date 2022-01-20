ad54cb2254ff5d1a46b3f9ae872161bfe5022241
Natalya <natasha-vd@yandex.ru>
2022-01-20 12:36:59 +0300

    add homework 1

---
+++ lesson-1/site-3/index.php
@@ -0,0 +1,12 @@
+<?php 
+$title = "Главная страница - страница обо мне";
+$header = "Информация обо мне";
+$date = date ('Y');
+
+$content = file_get_contents("template.php");
+$strings_template = array("{{ title }}", "{{ header }}", "{{ date }}");
+$variables_program = array($title, $header, $date);
+
+$content = str_replace($strings_template, $variables_program, $content);
+
+echo $content;
